787d6f7131ed0d1a99478bfe8be6b00b40cd2619
Abrosss <nataroro5@gmail.com>
2022-10-19 10:06:23 +0600

    fixed

--- routes/main.js
+++ routes/main.js
@@ -3,15 +3,9 @@ const router = express.Router();
 const mainController = require("../controllers/main");
 
 
-//Main Routes - simplified for now
+
 router.get("/", mainController.getHome);
 router.get("/about", mainController.getAbout);
 router.get("/plan", mainController.getPlan);
-// router.get("/feed", ensureAuth, postsController.getFeed);
-// router.get("/login", authController.getLogin);
-// router.post("/login", authController.postLogin);
-// router.get("/logout", authController.logout);
-// router.get("/signup", authController.getSignup);
-// router.post("/signup", authController.postSignup);
 
 module.exports = router;
